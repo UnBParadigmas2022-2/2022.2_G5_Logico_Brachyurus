6638fd0f470dd488006d9540838e852524d05333
Christian Fleury <chfleurysiq@gmail.com>
2022-12-24 08:27:22 -0300

    feat: start menu

---
+++ main.pl
@@ -0,0 +1,14 @@
+menu:-
+    write('----------------- BACHYURUS -----------------'),nl,
+    write('1 - História'),nl,
+    write('2 - Tutorial'),nl,
+    write('3 - Jogar'),nl,
+    write('4 - Sair'),nl,
+    read_string(user, "\n", "\r", _, X),
+	option(X),
+	X==0,
+	!.
+
+option(0):- !.
+option(1):- write('opcao 1'), nl, !.
+
