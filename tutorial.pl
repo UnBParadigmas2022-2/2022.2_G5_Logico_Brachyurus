704d46f4a1d058e848bb143a5b3b8e22934aeaeb
Christian Fleury <50469574+chfleury@users.noreply.github.com>
2022-12-30 00:40:42 -0300

    feat: add more instructions

--- tutorial.pl
+++ tutorial.pl
@@ -24,4 +24,8 @@ tutorial:-
     write('    _.                      :\\'),nl,
     write('                            ;`-\\'),nl,
     write('                           :\\'),nl,
-    write('                           ;\\'),nl.
+    write('                           ;\\'),nl, nl, nl,
+    write('Como jogar Brachyurus?'), nl,
+    write('Avance pela história selecionando uma das opções disponíveis, a fim de sobreviver nessa incrível jornada!'), nl,
+    write('Cada alternativa será representada por um número inteiro (1, 2, 3, 4...), basta escolher e digitar no terminal.'), nl,
+    write('Digite sempre o número da alternativa seguido por um ponto final. Ex: 2.'), nl.
